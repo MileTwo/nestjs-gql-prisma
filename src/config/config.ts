1d3328741fccd1b591c99a4f3312f5d83fd32298
John Fay <46365891+keonik@users.noreply.github.com>
2021-10-25 12:16:33 -0400

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export const config = () => ({
     app: {
         // host: process.env.HOST,
-        port: parseInt(process.env.NEST_PORT || 5000),
+        port: parseInt(process.env.NEST_PORT || '5000'),
     },
     // database: {},
 });
